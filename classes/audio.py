5117e19412a1af5a65a2df4493ce467f4e2d629e
Mostafaali3 <mostafaalisarhan@gmail.com>
2024-12-25 11:07:15 +0200

    initialization

---
+++ classes/audio.py
@@ -0,0 +1,28 @@
+class Audio():
+    def __init__(self):
+        self.data = None
+        self.spectrogram = None
+        self.hashing_result = []
+        self.sampling_rate = 22100 # arbitrary value 
+        
+    def preprocess_data(self):
+        pass
+    
+    def hash_sound(self):
+        pass 
+    
+    def change_sample_rate(self):
+        pass
+    
+    def cut_audio(self):
+        pass
+    
+    def generate_spectrogram(self):
+        pass
+    
+    def get_features(self):
+        pass 
+    
+    def compare(self):
+        pass
+    
